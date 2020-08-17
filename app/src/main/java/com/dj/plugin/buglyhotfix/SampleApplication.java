bbde42a77c65a902092ad364bdc0d01edf9b7d6f
wangjing <wangjing@dodoca.com>
2020-08-17 14:50:18 +0800

    完成测试

--- app/src/main/java/com/dj/plugin/buglyhotfix/SampleApplication.java
+++ app/src/main/java/com/dj/plugin/buglyhotfix/SampleApplication.java
@@ -1,5 +1,7 @@
 package com.dj.plugin.buglyhotfix;
 
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.tinker.loader.app.TinkerApplication;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
@@ -7,5 +9,11 @@ public class SampleApplication extends TinkerApplication {
     public SampleApplication() {
         super(ShareConstants.TINKER_ENABLE_ALL, "com.dj.plugin.buglyhotfix.SampleApplicationLike",
                 "com.tencent.tinker.loader.TinkerLoader", false);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bugly.init(getApplicationContext(), "a7f289d595", true);
     }
 }
